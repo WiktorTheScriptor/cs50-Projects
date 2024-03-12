efc3ee6bb1b791afff7c7788a3b7dcaabb786b6f
Wiktor Wrazka <wrazkaw@gmail.com>
2024-03-12 19:42:38 +0000

    added credit card project

---
+++ credit.c
@@ -0,0 +1,65 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+bool check(long card);
+
+int main(void)
+{
+    long card = get_long("enter card number: ");
+    // check if propper number
+    int test = card / 1000000000000;
+    string type = "INVALID\n";         // sets invalid default incase no if condition is true
+    if (test == 4 || test / 1000 == 4) // visa
+    {
+        type = "VISA\n";
+    }
+    else if (test / 10 == 34 || test / 10 == 37) // AMEX
+    {
+        type = "AMEX\n";
+    }
+    else if ((test / 100 >= 51) && (test / 100 <= 55)) // MASTER
+    {
+        type = "MASTERCARD\n";
+    }
+    if (check(card) && strcmp(type, "INVALID\n") != 0)
+    {
+        printf("%s", type);
+    }
+    else
+    {
+        printf("INVALID\n");
+    }
+}
+
+bool check(long card)
+{
+    long i = 10;
+    int evens = 0;
+    do
+    {
+        int num = 2 * ((card % (10 * i)) / i);
+        evens += num % 10;
+        evens += num / 10;
+        i *= 100;
+    }
+    while (card / i != 0);
+    card *= 10;
+    i = 10;
+    int odds = 0;
+    do
+    {
+        int num = ((card % (10 * i)) / i);
+        odds += num;
+        i *= 100;
+    }
+    while (card / i != 0);
+    if ((odds + evens) % 10 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
